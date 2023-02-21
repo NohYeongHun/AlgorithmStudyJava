f7aa65215dc77c0f50f7735a0dd97d655a0d9ef6
 NohYeongHun <dkzndk1100@naver.com>
2023-02-21 16:59:36 +0900

    기본 입출력 설정 추가
    백준 전용 입출력 코드 추가

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException {
+        // 한줄씩 읽어옴
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            Integer A = Integer.parseInt(st.nextToken());
+            Integer B = Integer.parseInt(st.nextToken());
+        }
     }
+
 }
